c1ca042a42b024e50a448a23053bd05c8a2b61cf
amirbaum <colocba@gmail.com>
2019-07-17 19:40:36 +0300

    Pic problems 10

--- src/components/LogosGrid.js
+++ src/components/LogosGrid.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Grid, Image, Divider, Container} from "semantic-ui-react";
-import callus from '../../public/assets/call-us-logo/call-us-logo.png';
 
 const style = {
     padding: '20px'
@@ -11,7 +10,7 @@ const LogosGrid = () => (
             <Grid verticalAlign="middle" columns={1} centered>
                 <Grid.Row>
                     <Grid.Column>
-                        <Image src={callus} size='medium' centered />
+                        <Image src="../assets/call-us-logo/call-us-logo.png" size='medium' centered />
                     </Grid.Column>
                 </Grid.Row>
                 <Divider/>
